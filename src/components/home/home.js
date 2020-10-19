cdb0655bee93fd24c63c2184cfd7213538760026
PixlKing <campagnamatias@gmail.com>
2020-10-18 23:41:50 -0300

    ajuste de permormance al volver a la home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -16,42 +16,36 @@ function Home(){
 
     useEffect(()=>{
         getComics()
-        onPageChange()
-    }, [])
+    }, [page])
 
     const getComics = async () => {
-        const data = await fetch(`https://gateway.marvel.com:443/v1/public/comics?offset=${page}&limit=10&apikey=633832778b0cb7f4ef7d6ed45d9bd2c1`);
-        const comics = await data.json()
-        setComics(comics.data.results)
-        setBusy(false)
-    }
-
-    const onPageChange = ()=>{
-		history.listen((location) => {
-			page = Number(location.pathname.replace("/",""));
-        	setBusy(true)
-			getComics()
-		})
+	    if(!isNaN(page)){
+			setBusy(true)
+			page = Number(window.location.pathname.replace("/",""));
+			const data = await fetch(`https://gateway.marvel.com:443/v1/public/comics?offset=${page}&limit=10&apikey=633832778b0cb7f4ef7d6ed45d9bd2c1`);
+			const comics = await data.json()
+			setComics(comics.data.results)
+			setBusy(false)
+	    }
     }
 
 	return(
 		<Fragment>
-	        <Header />
-	        <div className="content">
-	            <section className="catalog">
-	            	{
-	            		Busy ? <Loading />
-	            		: comics.map((comic)=>{
-	                        return(
-	                            <ComicBox key={comic.id} id={comic.id} title={comic.title} img={comic.thumbnail} />
-	                        )
-		                })
-	            	}
-
-	            </section>
-	        </div>
-	        <Footer />
-	    </Fragment>
+	       <Header />
+	       <div className="content">
+				<section className="catalog">
+					{
+						Busy ? <Loading />
+						: 	comics.map((comic)=>{
+								return(
+									<ComicBox key={comic.id} id={comic.id} title={comic.title} img={comic.thumbnail} />
+								)
+							})
+					}
+				</section>
+	       </div>
+	       <Footer />
+	   </Fragment>
 	)
 }
 
